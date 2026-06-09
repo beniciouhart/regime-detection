4e6ce25dee001d60f52c0bd57276b0b5ac100d62
Benicio Uhart <beniciouhart1@gmail.com>
2026-06-08 23:36:33 -0400

    further changes to features

--- features/features.py
+++ features/features.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 
 
-def compute_returns(df=get_merged_df(), series_ids= ['RBRTE','RWTC']):
+def compute_returns(df, series_ids= ['RBRTE','RWTC']):
     df = df.copy()
     for id in series_ids:
         df[id + '_log_return'] = np.log(df[id]/ df[id].shift(1))
